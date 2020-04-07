ca0c978fa6fc4eaa43730a9b5b492b3dc49bcb71
Jaymie Ruddock <jruddock@smu.edu>
2020-04-07 16:28:03 -0600

    working on login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home/home';
+import Login from './components/login/login';
 
 function App() {
     return (
-       <Home/>
+      <Home/>
     );
   }
 
--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Jumbotron } from 'react-bootstrap';
 
 
 class Login extends React.Component {
@@ -13,18 +14,35 @@ class Login extends React.Component {
 
     render() {
         return <>
-        <div className='login-container'>
-            <div className='login-container-box' id='login-box'>
-                <form className='login-form'>
-                    <div>
-                        <h1>Log In!</h1>
-                    </div>
-                    <div>
+        <Container className="justify-content-md-center" id="loginForm">
+            <Jumbotron fluid style={{width:'100%'}} className="bg-light" id="form-content">
+            <Container>
+                <Row className="formTitle">
+                    <h3>Login</h3>
+                </Row>
+                <Row className="bg-light">
+                    <form id='login-form'>
+                        <div>
+                            <Row>
+                            <label for='email-login'> Email </label>
+                            </Row>
+                            <input type="text" id='email-login' value={this.state.email} onChange={ e => this.setState({ email: e.target.value })}></input>
+                        </div>
+                        <div>
+                            <Row>
+                                <label for="password-login">Password</label>
+                            </Row>
+                            <input type='password' id='password-login' value={ this.state.password } onChange={e => this.setState({ password: e.target.value})}></input>
+                        </div>
+                        <div>
+                            <button type="submit" onClick={ () => this.onReviewSubmit() }>Submit</button>
+                        </div>
+                    </form>
+                </Row>
+            </Container>
 
-                    </div>
-                </form>
-            </div>
-        </div>
+            </Jumbotron>
+        </Container>
         </>;
     }
 }
