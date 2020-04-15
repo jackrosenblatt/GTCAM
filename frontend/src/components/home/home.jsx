ce0ba8798971a2f74d289ce86138ef7e6f319ef4
mariaherediaa <mbheredia@smu.edu>
2020-04-14 21:10:38 -0500

    updated dashboard

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -11,14 +11,81 @@ export class Home extends React.Component {
 
     render() {
         return <>
-            <Nav></Nav>
+             <Nav></Nav>
+
             <div><Calendar id='userCal'></Calendar></div>
             <p></p>
-            <Link to="/medicalinfo">Medical Info</Link> <p></p>
-            <Link to="/appointment">Go to Appointment</Link> <p></p>
-            <Link to="/prescriptions">Go to prescriptions</Link> <p></p>
-            <Link to="/pharmacies">Show List of Pharmacies</Link> <p></p>
-            <Link to="/message">Message Box</Link> 
+
+            <div class="container">
+
+                <div class="card text-center">
+                    <div class="card-header font-weight-bold">
+                        Patient Medical Infomation
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text" >Here you will find your personal information, your doctor's name, 
+                                            your medical information, and add current allergies. <br/>
+                                            Click the link below to see your medical information!</p>
+                        <a href="/medicalinfo" class="btn btn-info">Click Here!</a>
+                    </div>
+                </div>
+                <p></p>
+
+            <div class="card text-center">
+                <div class="card-header font-weight-bold">
+                    Patient Calendar
+                </div>
+
+                <div class="card-body">
+                    <p class="card-text"> Here you can check for upcoming appointments, make an appointment,
+                                        cancel an appointmnent, as well as view past appointments. <br/>
+                                        Click the link below to see your calendar and appointments!</p>
+                    <a href="/appointment" class="btn btn-info">Click Here!</a>
+                </div>
+            </div>
+            <p></p>
+
+            <div class="card text-center">
+                <div class="card-header font-weight-bold">
+                    Patient Prescriptions
+                </div>
+
+                <div class="card-body">
+                    <p class="card-text"> Here you can see past prescriptions, current prescriptions, and 
+                                        prescriptions that are ready to pick up. <br/>
+                                        Click the link below to see all of your prescriptions!</p>
+                    <a href="/prescriptions" class="btn btn-info">Click Here!</a>
+                </div>
+            </div>
+            <p></p>
+
+            <div class="card text-center">
+                <div class="card-header font-weight-bold">
+                    Patient Prefered Pharmacies
+                </div>
+
+                <div class="card-body">
+                    <p class="card-text"> Here you can see your prefered pharmacies and their current address and store hours. <br/>
+                                        Click the link below to see your prefered pharmacies!</p>
+                    <a href="/pharmacies" class="btn btn-info">Click Here!</a>
+                </div>
+            </div>
+            <p></p>
+
+            <div class="card text-center">
+                <div class="card-header font-weight-bold">
+                    Patient Message Log
+                </div>
+
+                <div class="card-body">
+                    <p class="card-text"> Here you can log any question or concerns about your prescriptions to a doctor or a pharmacit.
+                                        Click the link below log any questons!</p>
+                    <a href="/message" class="btn btn-info">Click Here!</a>
+                </div>
+            </div>
+            <p></p>
+            
+            </div>
         </>;
     }
 }
