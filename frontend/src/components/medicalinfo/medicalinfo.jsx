df06a68d9f007e460623ad41f466ade7a6885f10
mariaherediaa <mbheredia@smu.edu>
2020-04-16 00:08:08 -0500

    im going to SCREAM I CANNOT GET THIS BUTTON TO WORK

--- frontend/src/components/medicalinfo/medicalinfo.jsx
+++ frontend/src/components/medicalinfo/medicalinfo.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import { Container, Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Patient } from '../../models/patient.js';
 
 export class MedicalInfo extends React.Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            allergies: []
+        };
+
+         this.addAllergy = this.addAllergy.bind(this);
+    }
+
+    addAllergy(e) {
+        if (this._inputElement.value !== "") {
+          var newAllergy = {
+            text: this._inputElement.value,
+            key: Date.now()
+          };
+       
+          this.setState((prevState) => {
+            return { 
+              items: prevState.allergies.concat(newAllergy) 
+            };
+          });
+         
+          this._inputElement.value = "";
+        }
+         
+        console.log(this.state.allergies);
+           
+        e.preventDefault();
+      }
+
     render() {
+        
         return <>
 
             <p></p>
 
             <Container>
-
+            <form onSubmit={this.addAllergy}>
             <div className="card card bg-light mb-3 border-secondary">
                 <div className="card-header font-weight-bold text-center bg-secondary text-light border-secondary mb-3">
                     <h4>Welcome to Your Medical Information!</h4>
                 </div>
+                
                 <div className="card-body">
                     <p className="card-text">
                             <b>Name: </b> patient.name <br/>
@@ -31,16 +64,19 @@ export class MedicalInfo extends React.Component {
                 </div>
             </div>
 
-            <p></p>
-
-            
             <div className="card border-secondary mb-3">
             <h5 className="card-header text-dark border-secondary mb-3">Add A New Allergy Below!</h5>
-                <form>
+                
                 <div className="card-body">
                     <div className="col-12">
                             <label htmlFor="comment"> <b>New Allegy:</b> </label>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3">
+                            <textarea 
+                                className="form-control" 
+                                id="exampleFormControlTextarea1" 
+                                rows="3"
+                                ref={(a) => this._inputElement = a}
+                                placeholder="enter new allergy"
+                            >
                             </textarea>
                             <p></p>
                     </div>
@@ -49,17 +85,17 @@ export class MedicalInfo extends React.Component {
                         <button
                             type="button"
                             className="btn btn-info"
-                            onClick={ () => this.onSubmit() }>
-                            Submit
+                            onSubmit={ (newAl) => this.addAllergy(newAl) }>
+                            Add
                         </button>
                     </div>
 
                 </div>
-                </form>
             </div>
 
             <p></p>
             <a href="/DashBoard" className="btn btn-primary"> Back to DashBoard</a>
+            </form>
             </Container>
 
         </>;
