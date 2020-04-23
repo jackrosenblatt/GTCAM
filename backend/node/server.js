1f8d0a7a0154ce8ee030d130b03c2e2f1b827a00
veritas919 <olackner@smu.edu>
2020-04-23 17:31:56 -0500

    Update server.js
    Gets for specific dates

--- backend/node/server.js
+++ backend/node/server.js
@@ -128,6 +128,25 @@ app.get('/appointments/patient/:id', (req,  res) => {
 	})
 })
 
+//get appointments for a patient on a given day
+app.get('/appointments/patient/specificDate/:id/:date', (req,res) =>{
+	var query = "select * from Appointments where patientID =\"" req.params.id+"\" and date=\""+req.params.date+"\"";
+
+	connection.query(query,function(err,result,fields){
+		switch(result.length){
+			case 0:
+				res.status(400).send("No Appointments On Selected Date");
+				return;
+			default:
+			res.status(200).send(result);
+			return; 
+		}
+
+	})
+
+})
+
+
 //get appointments for a doctor
 app.get('/appointments/doctor/:id', (req,  res) => {
 	var query = "select * from Appointments where docID=\""+req.params.id+"\"";
@@ -142,6 +161,24 @@ app.get('/appointments/doctor/:id', (req,  res) => {
 				return;
 		}
 	})
+})
+
+//get appointments for a doctor on a given day
+app.get('/appointments/doctor/specificDate/:id/:date', (req,res) =>{
+	var query = "select * from Appointments where docID =\"" req.params.id+"\" and date=\""+req.params.date+"\"";
+
+	connection.query(query,function(err,result,fields){
+		switch(result.length){
+			case 0:
+				res.status(400).send("No Appointments On Selected Date");
+				return;
+			default:
+			res.status(200).send(result);
+			return; 
+		}
+
+	})
+
 })
 
 //get appointments for a doctor patient combo
