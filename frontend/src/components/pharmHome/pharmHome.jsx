610abe7b44d4ebbda52cb93fc74dec889e8c4cb3
Jaymie Ruddock <jruddock@smu.edu>
2020-04-27 16:33:28 -0600

    last changes for pharmacist i think?

--- frontend/src/components/pharmHome/pharmHome.jsx
+++ frontend/src/components/pharmHome/pharmHome.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Calendar from 'react-calendar';
 import PharmNav from '../pharmNav/pharmNav'; 
 import { Redirect } from 'react-router-dom';
+import './pharmhome.css';
 
 export class PharmHome extends React.Component {
     constructor(props) {
@@ -30,25 +31,25 @@ export class PharmHome extends React.Component {
 
                 <div class="card text-center">
                     <div class="card-header font-weight-bold">
-                        All Patients 
+                        Medications 
                     </div>
                     <div class="card-body">
-                        <p class="card-text" >  Here you will find all the patients picking up a medication at your pharmacy. <br/>
-                                                Click the link below to see your all of the patients!</p>
-                        <a href="" class="btn btn-info">Click Here!</a>
+                        <p class="card-text" >  Here you will find all the medications available at your pharmacy. <br/>
+                                                Click the link below to see medications or enter a new one!</p>
+                        <a href="/Pharm/medications" id='pharm-med-home' class="btn btn-primary">Click Here!</a>
                     </div>
                 </div>
                 <p></p>
 
                 <div class="card text-center">
                     <div class="card-header font-weight-bold">
-                        Prescriptions
+                        Pharmacies
                     </div>
 
                     <div class="card-body">
-                        <p class="card-text"> Here is a list of all the medications that need to be made<br/>
-                                            Click the link below to see all of the perscriptions</p>
-                        <a href="/Pharm/medications" class="btn btn-info">Click Here!</a>
+                        <p class="card-text"> Here is a list of all the pharmacies that you have access to.<br/>
+                                            Click the link below to see more information about them as well as their inventories!</p>
+                        <a href="/Pharm/pharmacies" id='pharm-pharm-home' class="btn btn-primary">Click Here!</a>
                     </div>
                 </div>
                 <p></p>
